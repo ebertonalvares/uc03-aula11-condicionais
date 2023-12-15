efe72d16b60cafadb3986aeb73a61f5245e6c1d5
eberton mauricio alvares <ebertonalvares@hotmail.com>
2023-12-15 19:41:17 -0300

    Primeiro Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*# Como criar um projeto com Node.js 
+1- Cria uma pasta nova para o projeto 
+2- Abre essa nova pasta dentro do vscode 
+3- Abrir o terminal no vscode e executar o comando "nmp init -y" para criar o arquivo "package.json"
+4- Criar uma pasta chamada "src" na raiz do projeto 
+5- Criar um arquivo "index.js" dentro da pasta "src"
+6- Inicializar o projeto como um repositorio Git executando o comando "git init"
+7- Criar um arquivo chamado ".gitignore" na raiz do projeto e incluir o texto "node_modules"
+8- Criar o primeiro commit 
+
+*/
+
